e25b4d31234cf1fdea8a2cf169e9efa22f856e36
Adam <adam.schachterle@gmail.com>
2019-03-12 13:01:39 -0400

    randomize function working

--- public/service.js
+++ public/service.js
@@ -2,7 +2,7 @@
 function FoodService($http, $location) {
     const self = this;
     self.searchRestID = (location) => {
-        console.log(location)
+        console.log(location, "this is the location that the user input")
         return $http({
             method: "GET",
             url: `https://developers.zomato.com/api/v2.1/locations?query=${location}
@@ -15,7 +15,7 @@ function FoodService($http, $location) {
         }).then((data) => {
             self.jsonPayload = data;
             self.cityID = self.jsonPayload.data.location_suggestions[0].entity_id
-            console.log(self.cityID)
+            console.log(self.cityID, "this is the city id that the API is returning")
             $location.path("/comptwo")
 
             return self.jsonPayload
@@ -28,20 +28,19 @@ function FoodService($http, $location) {
 
     
     
-    self.searchRest = (posts, opt1, opt2, opt3, opt4, opt5) => {
-        console.log(posts, "this is posts")
+    self.searchRest = (id, opt1, opt2, opt3, opt4, opt5) => {
         return $http({
             method: "GET",
-            url: `https://developers.zomato.com/api/v2.1/search?entity_id=285&entity_type=${posts}&cuisines=${opt1}%2C%20${opt2}%2C%20${opt3}%2C%20${opt4}%2C%20${opt5}&sort=rating&order=desc
+            url: `https://developers.zomato.com/api/v2.1/search?entity_id=285&entity_type=${id}&cuisines=${opt1}%2C%20${opt2}%2C%20${opt3}%2C%20${opt4}%2C%20${opt5}&sort=rating&order=desc
 
             `,
             headers: {
                 "user-key": "542769761b546b1e16b0443af117653d"
             }            
-        }).then((moredata2) => {
-            self.jsonPayloadThird = moredata2;
-            console.log(self.jsonPayloadThird, "jsonPayloadThird")
-            return self.jsonPayloadThird
+        }).then((moredata) => {
+            self.jsonPayload2 = moredata;
+            console.log(self.jsonPayload2, "jsonPayloadTwo")
+            return self.jsonPayload2
         })
     }
     
